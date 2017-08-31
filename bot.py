c1841c28cc94014f9b76a15129f8022e85ea4755
Sanguchi <22matias1994@gmail.com>
2017-08-30 23:04:08 -0300

    Added bot

---
+++ bot.py
@@ -0,0 +1,33 @@
+from os import environ
+import telebot
+
+
+print("Loading bot module")
+
+
+if("TELEGRAM_TOKEN" in environ):
+    token = environ["TELEGRAM_TOKEN"]
+else:
+    exit("Environment variable TELEGRAM_TOKEN not set, please add it using 'heroku config:set TELEGRAM_TOKEN=<TOKEN>'")
+
+
+if("HEROKU_URL" in environ):
+    url = environ["HEROKU_URL"]
+    # Handle missing slash
+    if(not url.endswith('/')):
+        url = url + '/'
+else:
+    exit("Environment variable HEROKU_URL not set, please add it using 'heroku config:set HEROKU_URL=<URL>'")
+
+
+bot = telebot.TeleBot(token)
+
+welcome_message = '''Hello!
+This bot is running on a heroku instance.'''
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, welcome_message)
+
+print("Setting webhook url to: ", url)
+bot.set_webhook(url)
--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 import flask
+import bot
+from telebot.types import Update
+
+
 app = flask.Flask(__name__)
+
 
 @app.route('/')
 def index():
 	return "It works!"
 
+
+@app.route("/{}".format(bot.token), methods=["POST"])
+def handle_update():
+    if(flask.request.headers.get("content-type") == "application/json"):
+        json_string = flask.request.get_data().decode("utf-8")
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+
 if(__name__ == "__main__"):
-	app.run()
+    app.run()
