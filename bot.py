bdf1cdbe6dd7b7c5bfbd89a8a375dcf796dd2cc9
Sanguchi <22matias1994@gmail.com>
2017-08-31 23:45:01 -0300

    Added comments and heroku labs support

--- bot.py
+++ bot.py
@@ -3,22 +3,26 @@ import telebot
 
 
 print("Loading bot module")
+
+
 if("HEROKU_APP_NAME" in environ):
     print("HEROKU_APP_NAME detected, value:", environ["HEROKU_APP_NAME"])
+    url = "https://{}.herokuapp.com/".format(environ["HEROKU_APP_NAME"])
+else:
+    print("HEROKU_APP_NAME not found, please enable it with 'heroku labs:enable runtime-dyno-metadata -a <APP_NAME>'")
+    print("trying HEROKU_URL...")
+    if("HEROKU_URL" in environ):
+        url = environ["HEROKU_URL"]
+        # Handle missing slash
+        if(not url.endswith('/')):
+            url = url + '/'
+    else:
+        exit("Environment variable HEROKU_URL not set, please add it using 'heroku config:set HEROKU_URL=<URL>'")
 
 if("TELEGRAM_TOKEN" in environ):
     token = environ["TELEGRAM_TOKEN"]
 else:
     exit("Environment variable TELEGRAM_TOKEN not set, please add it using 'heroku config:set TELEGRAM_TOKEN=<TOKEN>'")
-
-
-if("HEROKU_URL" in environ):
-    url = environ["HEROKU_URL"]
-    # Handle missing slash
-    if(not url.endswith('/')):
-        url = url + '/'
-else:
-    exit("Environment variable HEROKU_URL not set, please add it using 'heroku config:set HEROKU_URL=<URL>'")
 
 
 welcome_message = '''Hello!
@@ -32,7 +36,7 @@ url = url + token
 bot = telebot.TeleBot(token)
 
 
-
+# Simple example of a command handler.
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     bot.reply_to(message, welcome_message)
